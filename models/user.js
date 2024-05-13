fc064c475d7cffcc96ff872616bf21d25de47e2d
Mang Tuấn Vĩ <viu10112000@gmail.com>
2024-05-13 14:22:58 +0700

    fist commit

--- models/user.js
+++ models/user.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
 const userSchema = new Schema(
     {
         email: { type: String },
         password: { type: String },
         coin: { type: Number },
-        localPass:{type:String}
+        localPass: { type: String },
+        isOpenVideo: { type: Boolean, default: false }, // Thuộc tính isOpenVideo
+        isOpenPhoto: { type: Boolean, default: false }, // Thuộc tính isOpenPhoto
+        isOpenGallery: { type: Boolean, default: false } // Thuộc tính isOpenGallery
     },
     {
         versionKey: false,
